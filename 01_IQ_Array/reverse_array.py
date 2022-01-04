daaf5fba412ff33392ef52e9d133f8be95d50c69
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2022-01-04 13:39:45 +0600

    array reverse

---
+++ 01_IQ_Array/reverse_array.py
@@ -0,0 +1,14 @@
+def reverse(nums):
+    start_index = 0
+    end_index = len(nums) - 1
+
+    while end_index > start_index:
+        nums[start_index], nums[end_index] = nums[end_index], nums[start_index]
+        start_index = start_index + 1
+        end_index = end_index - 1
+
+
+if __name__ == '__main__':
+    n = [25, 5, 63, 21, 54]
+    reverse(n)
+    print(n)
